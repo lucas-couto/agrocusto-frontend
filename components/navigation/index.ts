81cbd313b9ede0737c4d94b31aa951540871c67c
Lucas Couto <contato@lucas-couto.com>
2026-04-24 12:10:02 -0300

    feat(nav): add barrel export for navigation components
    Exports: Sidebar, BottomNav, MoreSheet, NavItemButton, NAV_ITEMS,
    and types NavItem, NavTab, NavSurface.

---
+++ components/navigation/index.ts
@@ -0,0 +1,6 @@
+export { Sidebar } from './sidebar';
+export { BottomNav } from './bottom-nav';
+export { MoreSheet } from './more-sheet';
+export { NavItemButton } from './nav-item';
+export { NAV_ITEMS } from './nav-config';
+export type { NavItem, NavTab, NavSurface } from './nav-config';
